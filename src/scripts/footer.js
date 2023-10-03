31ed5a6b48f0cdb969455a22196ba6fe68d1cb52
edonssfall <tsarov13leo@gmail.com>
2023-10-03 13:58:24 +0200

    edit design and support modal func

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -5,3 +5,19 @@ document.getElementById('openContactsModal').addEventListener('click', () => {
 document.getElementById('openSupportModal').addEventListener('click', () => {
     $('#supportModal').modal('show');
 });
+
+const supportForm = document.getElementById('supportForm');
+const sendButton = document.getElementById('sendButton');
+
+sendButton.addEventListener('click', function () {
+    const formData = new FormData(supportForm);
+    let message = "Ihre Nachricht wurde erfolgreich gespeichert.\n";
+
+    formData.forEach((value, key) => {
+        message += key + ":" + value + "\n";
+    });
+
+    alert(message);
+
+    supportForm.reset();
+});
