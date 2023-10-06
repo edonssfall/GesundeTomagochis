59412d0e0c5e3f830c8bf73c88fff0817cd82c65
edonssfall <tsarov13leo@gmail.com>
2023-10-06 11:05:58 +0200

    add translate

--- src/scripts/demo.js
+++ src/scripts/demo.js
@@ -7,12 +7,43 @@ const scoreValue = document.getElementById('scoreValue'),
     tomagochiDescription = document.getElementById('tomagochiDescription'),
     urlParams = new URLSearchParams(window.location.search),
     urlImage = urlParams.get('demo'),
-    imgPath = './src/img/';
+    imgPath = './src/img/',
+    themeToggle = document.getElementById('theme-toggle-checkbox');
 
 let buttonsText = [],
     score = 0,
+    tomagochiAlertTitel = [],
     tomagochisGeschichte = [],
-    startImage = '';
+    startImage = '',
+    currentLanguage = 'en';
+
+themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+        document.body.classList.remove('dark-mode');
+    } else {
+        document.body.classList.add('dark-mode');
+    }
+});
+
+document.getElementById('en-language').addEventListener('click', () => {
+    currentLanguage = 'en';
+    if (urlImage === 'rauchen') {
+        makeRauchenLanguage();
+    } else {
+        makeSpielLanguage();
+    }
+    nameButtons()
+});
+
+document.getElementById('de-language').addEventListener('click', () => {
+    currentLanguage = 'de';
+    if (urlImage === 'rauchen') {
+        makeRauchenLanguage();
+    } else {
+        makeSpielLanguage();
+    }
+    nameButtons();
+});
 
 document.getElementById('minus10Button').addEventListener(
     'click', () => {
@@ -62,36 +93,65 @@ function createImage() {
             scoreImage.classList.add('rauchen_img');
             mainImage.src = imgPath + 'smartWatch_upscaled.jpg';
             mainImage.style.width = "50%";
-            buttonsText = ['Laufen(+10)', 'Spazieren(+5)', 'Rauchen(-10)'];
-            tomagochisGeschichte = ['Ihr Tomagotchi hat den Punkt erreicht, an dem es nicht mehr weitergehen konnte. Die Sucht nach dem Rauchen hat ihm schwer zugesetzt, und trotz Ihrer Bemühungen, es gesund zu halten, hat das Rauchen letztendlich gesiegt. Es ist eine wichtige Erinnerung daran, wie gefährlich und zerstörerisch Rauchen sein kann, nicht nur für Menschen, sondern auch für unsere virtuellen Freunde. Möge es in Frieden ruhen.',
-                "Herzlichen Glückwunsch! Du hast es geschafft, deine Rauchgewohnheiten erfolgreich zu überwinden. Dies ist eine beeindruckende Leistung, die zeigt, dass du die Kontrolle über dein Leben zurückgewonnen hast. Deine Entschlossenheit und Willenskraft verdienen Anerkennung. Jetzt kannst du stolz auf dich sein und dein Leben in vollen Zügen genießen, ohne von dieser Sucht eingeschränkt zu werden. Mach weiter so und bleibe stark!"
-            ];
+            makeRauchenLanguage();
         } else if (urlImage === 'spielen') {
             scoreImage.src = imgPath + 'spielen/image0.jpg';
             scoreImage.classList.add('spielen_img');
             mainImage.src = imgPath + 'bildschirm_upscaled.jpg'
             mainImage.style.width = "89%";
-            buttonsText = ['Lernen(+10)', 'Anschauen(+5)', 'Spielen(-10)'];
-            tomagochisGeschichte = ['Ihr Tomagotchi ist in seinem unermüdlichen Streben nach stundenlangem Spielen am PC an seine Grenzen gestoßen. Die exzessive Bildschirmzeit und Vernachlässigung seiner Bedürfnisse haben zu seinem tragischen Ende geführt. Dies sollte uns daran erinnern, wie wichtig es ist, eine ausgewogene Balance zwischen virtuellem Vergnügen und der realen Welt zu finden. R.I.P., kleiner Freund.',
-                "Herzlichen Glückwunsch! Du hast es geschafft, dich von exzessivem Spielen am PC zu befreien. Dies ist eine beeindruckende Leistung, die zeigt, dass du die Kontrolle über dein Leben zurückgewonnen hast. Deine Entschlossenheit und Willenskraft verdienen Anerkennung. Jetzt kannst du stolz auf dich sein und dein Leben in vollen Zügen genießen, ohne von dieser Sucht eingeschränkt zu werden. Mach weiter so und bleibe stark!"
-            ];
+            makeSpielLanguage();
         } else {
             demoBlock.style.display = 'none';
             return;
         }
-        const buttons = [
-            plus10Button,
-            plus5Button,
-            minus10Button
-        ];
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].textContent = buttonsText[i];
-        }
+        nameButtons();
         enterBlock.style.display = 'none';
     } else {
         demoBlock.style.display = 'none';
     }
 }
+
+function makeSpielLanguage() {
+    if (currentLanguage === 'en') {
+        buttonsText = ['Lernen(+10)', 'Anschauen(+5)', 'Spielen(-10)'];
+        tomagochisGeschichte = ['Ihr Tomagotchi ist in seinem unermüdlichen Streben nach stundenlangem Spielen am PC an seine Grenzen gestoßen. Die exzessive Bildschirmzeit und Vernachlässigung seiner Bedürfnisse haben zu seinem tragischen Ende geführt. Dies sollte uns daran erinnern, wie wichtig es ist, eine ausgewogene Balance zwischen virtuellem Vergnügen und der realen Welt zu finden. R.I.P., kleiner Freund.',
+            "Herzlichen Glückwunsch! Du hast es geschafft, dich von exzessivem Spielen am PC zu befreien. Dies ist eine beeindruckende Leistung, die zeigt, dass du die Kontrolle über dein Leben zurückgewonnen hast. Deine Entschlossenheit und Willenskraft verdienen Anerkennung. Jetzt kannst du stolz auf dich sein und dein Leben in vollen Zügen genießen, ohne von dieser Sucht eingeschränkt zu werden. Mach weiter so und bleibe stark!"
+        ];
+    } else {
+        buttonsText = ['Learn(+10)', 'Watch(+5)', 'Play(-10)'];
+        tomagochisGeschichte = ["Your Tomagotchi has reached its limits in its relentless pursuit of hours of PC gaming. Excessive screen time and neglect of its needs have led to its tragic demise. This should remind us of the importance of finding a balanced equilibrium between virtual enjoyment and the real world. R.I.P., little friend.",
+            "Congratulations! You've managed to free yourself from excessive PC gaming. This is an impressive achievement that shows you've regained control of your life. Your determination and willpower deserve recognition. Now you can be proud of yourself and enjoy your life to the fullest without being restricted by this addiction. Keep it up and stay strong!"
+        ];
+    }
+}
+
+function makeRauchenLanguage() {
+    if (currentLanguage === 'de') {
+        buttonsText = ['Laufen(+10)', 'Spazieren(+5)', 'Rauchen(-10)'];
+        tomagochiAlertTitel = ['Tomagochi ist gestorben', 'Tomagochi gratuliert Ihnen!!!'];
+        tomagochisGeschichte = ['Ihr Tomagotchi hat den Punkt erreicht, an dem es nicht mehr weitergehen konnte. Die Sucht nach dem Rauchen hat ihm schwer zugesetzt, und trotz Ihrer Bemühungen, es gesund zu halten, hat das Rauchen letztendlich gesiegt. Es ist eine wichtige Erinnerung daran, wie gefährlich und zerstörerisch Rauchen sein kann, nicht nur für Menschen, sondern auch für unsere virtuellen Freunde. Möge es in Frieden ruhen.',
+            "Herzlichen Glückwunsch! Du hast es geschafft, deine Rauchgewohnheiten erfolgreich zu überwinden. Dies ist eine beeindruckende Leistung, die zeigt, dass du die Kontrolle über dein Leben zurückgewonnen hast. Deine Entschlossenheit und Willenskraft verdienen Anerkennung. Jetzt kannst du stolz auf dich sein und dein Leben in vollen Zügen genießen, ohne von dieser Sucht eingeschränkt zu werden. Mach weiter so und bleibe stark!"
+        ];
+    } else {
+        buttonsText = ['Run(+10)', 'Walk(+5)', 'Smoke(-10)'];
+        tomagochiAlertTitel = ['Tomagotchi has died', 'Tomagotchi congratulates you!!!'];
+        tomagochisGeschichte = ["Your Tomagotchi reached a point where it couldn't go on any longer. The addiction to smoking took a toll on it, and despite your efforts to keep it healthy, smoking ultimately prevailed. It's an important reminder of how dangerous and destructive smoking can be, not only for humans but also for our virtual friends. May it rest in peace.",
+            "Congratulations! You've successfully overcome your smoking habits. This is an impressive achievement that shows you've regained control of your life. Your determination and willpower deserve recognition. Now you can be proud of yourself and enjoy your life to the fullest without being restricted by this addiction. Keep it up and stay strong!"
+        ];
+    }
+}
+
+function nameButtons() {
+    const buttons = [
+        plus10Button,
+        plus5Button,
+        minus10Button
+    ];
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].textContent = buttonsText[i];
+    }
+}
+
 
 function checkImage() {
     if (urlImage === 'rauchen') {
@@ -103,14 +163,14 @@ function checkImage() {
         scoreImage.src += 'image0.jpg'
     } else if (score >= 100) {
         scoreImage.src += 'image100.jpg';
-        tomagochiTitel.textContent = 'Tomagochi gratuliert Ihnen!!!'
+        tomagochiTitel.textContent = tomagochiAlertTitel[1]
         tomagochiDescription.textContent = tomagochisGeschichte[1]
         $('#alertModal').modal('show');
     } else if (score >= 10) {
         scoreImage.src += 'image10.jpg';
     } else if (score <= -250) {
         scoreImage.src += 'image-250.jpg';
-        tomagochiTitel.textContent = 'Tomagochi ist gestorben'
+        tomagochiTitel.textContent = tomagochiAlertTitel[0]
         tomagochiDescription.textContent = tomagochisGeschichte[0]
         $('#alertModal').modal('show');
     } else if (score <= -100) {
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,37 @@
 const linedTextElement = document.querySelector('.lined-text'),
     stickMan = document.querySelector('.stick-man'),
-    sections = gsap.utils.toArray(".section");
+    sections = gsap.utils.toArray(".section"),
+    themeToggle = document.getElementById('theme-toggle-checkbox');
 
-let tops = sections.map(section => ScrollTrigger.create({trigger: section, start: "top top"}));
+let tops = sections.map(section => ScrollTrigger.create({trigger: section, start: "top top"})),
+    currentLanguage = 'de',
+    languages = {};
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const imgElements = document.querySelectorAll('img');
+    imgElements.forEach(img => {
+        img.setAttribute('loading', 'lazy');
+    });
+});
+
+themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+        document.body.classList.add('light-mode');
+    } else {
+        document.body.classList.remove('light-mode');
+    }
+});
+
+document.getElementById('en-language').addEventListener('click', () => {
+    currentLanguage = 'en';
+    loadLanguage(currentLanguage);
+});
+
+document.getElementById('de-language').addEventListener('click', () => {
+    currentLanguage = 'de';
+    loadLanguage(currentLanguage);
+});
 
 function addLineThrough() {
     linedTextElement.style.textDecoration = 'line-through';
@@ -18,6 +47,33 @@ function apearStickMan() {
 
 function hideStickMan() {
     stickMan.style.display = 'none';
+}
+
+function updateText() {
+    const elementsToUpdate = document.querySelectorAll('[data-translate]');
+    elementsToUpdate.forEach((element) => {
+        const key = element.getAttribute('data-translate');
+        if (languages[currentLanguage] && languages[currentLanguage][key]) {
+            element.textContent = languages[currentLanguage][key];
+        }
+    });
+}
+
+function loadLanguage(language) {
+    fetch(`./src/language/${language}.json`)
+        .then((response) => response.json())
+        .then((data) => {
+            languages[language] = data;
+            updateText();
+
+            const placeholders = languages[language];
+            document.getElementById('name').setAttribute('placeholder', placeholders.name_placeholder);
+            document.getElementById('email').setAttribute('placeholder', placeholders.email_placeholder);
+            document.getElementById('question').setAttribute('placeholder', placeholders.question_placeholder);
+        })
+        .catch((error) => {
+            console.error('Error loading language:', error);
+        });
 }
 
 gsap.registerPlugin(ScrollTrigger);
@@ -66,12 +122,6 @@ ScrollTrigger.create({
     },
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    const imgElements = document.querySelectorAll('img');
-    imgElements.forEach(img => {
-        img.setAttribute('loading', 'lazy');
-    });
-});
-
 $("#footer").load("footer.html");
+
+loadLanguage(currentLanguage);
